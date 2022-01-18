36c9fd785c79def95bff8efbcd90affb35022a6a
Sara <95581411+saraegregg@users.noreply.github.com>
2022-01-18 12:56:50 -0600

    Adding the election audit code.

--- PyPoll.py
+++ PyPoll.py
@@ -8,29 +8,78 @@
 # #import the CVS module and the os module
 import os
 import csv
-# # In order to list what is in the module use dir(), example:
-# # dir(csv)
 
-# #Assign a variable for the file to load and the path.
-file_to_load = os.path.join('resources', 'election_results.csv')
-#Create a filename variable to an indirect path to the file.
-file_to_save = os.path.join('analysis', 'election_analysis.txt')
+# Assign a variable to load a file from a path.
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+file_to_load = os.path.join("Resources/election_results.csv")
+# Assign a variable to save the file to a path.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+#Initialize a variable accumulator to count the total votes
+total_votes = 0
+
+#Declare a candidate list
+candidate_options = []
+
+#Declare a candidate vote count dictionary
+candidate_votes = {}
+
+#Initialize winning candidate and winning count tracker
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 #Open the election results and read the file.
 with open(file_to_load) as election_data:
 
-    #Read and analyze the data here.
     #Read the file object with the reader function.
     file_reader = csv.reader(election_data)
 
-    #Print the header row
+    #Read the header row
     headers = next(file_reader)
-    print(headers)
 
+    #Print each row in the CSV file:
+    for row in file_reader:
+            
+        #Add +1 to total votes
+        total_votes += 1
 
-# #Using the open() function with the "w" mode, we will write data to the file
-# with open(file_to_save, "w") as txt_file:
+        #Get the candidates' names from the row and add it to the list if not already there
+        candidate_name = row[2]
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
 
-#     #Write some data to the file
-#     txt_file.write("Counties in the Election\n-------------------------\nArapahoe\nDenver\nJefferson")
+            #Add candidate name as a key in the dictionary (later to add each vote under their name)
+            candidate_votes[candidate_name] = 0
+        
+        #Add +1 to respective candidate vote total
+        candidate_votes[candidate_name] += 1
+    
+    #Calculate each candidate's percentage of votes by:
+    #Iterate through the candidate list
+    for candidate_name in candidate_votes:
 
+        #Retrieve vote count of a candidate.
+        votes = candidate_votes[candidate_name]
+
+        #Calculate the percentage of votes:
+        vote_percentage = (float(votes) / float(total_votes)) * 100
+
+        #Determine winning vote count and candidate/determine if the votes is greater than the winning count.
+        if (votes > winning_count) and (vote_percentage > winning_percentage):
+            #if true set winning_count = votes and winning_percentage = vote percentage and candidate's name to winning_candidate
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = candidate_name
+        
+        #Print the candidate name and percentage of votes.
+        print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+
+    #print the winning candidate, vote count, and percentage
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
